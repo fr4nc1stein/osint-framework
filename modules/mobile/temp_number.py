91ad1565d47082b6992ddc8fba9a6381ec19070f
James Zerdick <ecjmaquic@gmail.com>
2022-10-16 23:14:36 +0800

    init

---
+++ modules/mobile/temp_number.py
@@ -0,0 +1,13 @@
+import requests
+from sploitkit import *
+
+class SearchMobile(Module):
+    """ This module with validate if the mobile number is temporary.
+    Author:  cadeath
+    Version: 1.0
+    """
+    source = [
+        'https://mytempsms.com/receive-sms-online/philippines-phone-number.html'
+    ]
+    
+    
