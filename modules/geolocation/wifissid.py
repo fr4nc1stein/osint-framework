71879b48e41a6ccfc282543049076807adf4ce29
James Zerdick <ecjmaquic@gmail.com>
2022-10-23 10:31:41 +0800

    update

--- modules/geolocation/wifissid.py
+++ modules/geolocation/wifissid.py
@@ -9,23 +9,18 @@ from dateutil.relativedelta import relativedelta
 from pygments import highlight, lexers, formatters
 
 
-class GeoWireless(Module):
-    """ This module searches the location of the given MAC Address.
+class GeoWifi(Module):
+    """ This module searches the information like BSSID, Location, and Encryption of the given SSID.
     https://wigle.net
     Author:  cadeath
     Version: 1.0
     """
     config = Config({
         Option(
-            "WIRELESS_IDENTIFIER",
-            "This could be MAC Address or SSID",
+            "WIRELESS_SSID",
+            "Enter exact Wifi SSID",
             True
-        ): str("XX:XX:XX:XX:XX:XX|SSID"),
-        Option(
-            "COUNTRY",
-            "Country where to lookup",
-            False,
-        ): str("PH")
+        ): str("PLDTHomeXXXX")
     })
 
     PH = {
@@ -42,25 +37,40 @@ class GeoWireless(Module):
         BasicAuth = b64encode(userToken.encode("utf-8"))
         BasicAuth = BasicAuth.decode("utf-8")
 
-        wirelessIdentifier = self.config.option('WIRELESS_IDENTIFIER').value
+        WifiSSID = self.config.option('WIRELESS_SSID').value
+        authHeaders = { 'Authorization': 'Basic ' + BasicAuth, 'Accept': 'application/json'}
 
         print("Searching...")
 
-        wifi_url, entryType = self._requestWifi(wirelessIdentifier)
-        r = requests.get(wifi_url, headers = { 'Authorization': 'Basic ' + BasicAuth, 'Accept': 'application/json'})
-        self._debug(r.json())
+        wifiUrl = self._requestWifiBSSID(WifiSSID)
 
-        tableData = []
+        authHeaders = { 'Authorization': 'Basic ' + BasicAuth, 'Accept': 'application/json'}
+        r = requests.get(wifiUrl, headers=authHeaders)
+        if r.status_code == 429:
+            print("[-] Too many requests, try again later or have a new API key")
+            return 
+
+        # self._debug(r.json())
+
+        tblDetails = []
         rep = r.json()
 
-        title = f"{entryType}: {wirelessIdentifier}"
-        th = ('Seen','Location','Coordinates')
-        tableData.append(th)
+        title = f"SSID: {WifiSSID}"
+        th = ('Key','Value')
+        tblDetails.append(th)
+        
+        if rep["resultCount"] < 0:
+            print("[-] SSID Information not found.")
+            return
+
+        tblGeo = []
+        th = ('Last Update','MAC','Encryption','Channel','Location', 'Coordinates')
+        tblGeo.append(th)
 
         for el in rep["results"]:
-            tableData.append((el["lastupdt"],f"{el['road']} {el['city']} {el['region']}",f"{el['trilat']} , {el['trilong']}"))
+            tblGeo.append((el["lastupdt"],f"{el['netid']}",f"{el['encryption']}",f"{el['channel']}",f"{el['road']} {el['city']} {el['region']}, {el['country']}",f"{el['trilat']} , {el['trilong']}"))
 
-        self._print(title,tableData)
+        self._print(title,tblGeo)
 
     def _formatCheck(self,entry):
         if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", entry.lower()):
@@ -87,22 +97,15 @@ class GeoWireless(Module):
         print(colorful)
 
     def _last6Mon(self):
-        six_months = date.today() + relativedelta(months=+6)
+        six_months = date.today() + relativedelta(months=-6)
         ret = six_months.strftime("%Y%m%d") + "000000"
+        
         return ret
 
-    def _requestWifi(self,id):
+    def _requestWifiBSSID(self,ssid):
         url = "https://api.wigle.net/api/v2/network/search?onlymine=false&"
-        url += url + f"latrange1={self.PH['Lat1']}&latrange2={self.PH['Lat2']}&longrange1={self.PH['Lon1']}&longrange2={self.PH['Lon2']}"
+        url += f"latrange1={self.PH['Lat1']}&latrange2={self.PH['Lat2']}&longrange1={self.PH['Lon1']}&longrange2={self.PH['Lon2']}"
         url += f"&lastupdt={self._last6Mon()}&freenet=false&paynet=false&country=PH"
-        
-        entryType = self._formatCheck(id)
-        if entryType == "MAC":
-            url += f"&netid={id}"
-        elif entryType == "SSID":
-            url += f"&ssid={id}"
+        url += f"&ssid={ssid}"
 
-        return url, entryType
-
-    def _requestBT(self,id):
-        pass
+        return url
