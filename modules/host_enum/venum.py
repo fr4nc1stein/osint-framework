eedac1bcca1a359ab75281db93d66c6558c30917
James Zerdick <ecjmaquic@gmail.com>
2022-11-12 03:44:24 +0800

    Update venum.py
    testing

--- modules/host_enum/venum.py
+++ modules/host_enum/venum.py
@@ -1,5 +1,11 @@
-import os,requests
-from sploitkit import Module, Config, Option, Model
+import logging
+import re,requests
+from sploitkit import Module, Config, Option
+
+DEBUG = False
+requests.packages.urllib3.disable_warnings() 
+logging.basicConfig(filename='module-error.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
 
 class Venum(Module):
     """ This module will check if the IP hosted a domain that is using a virtualhost
@@ -36,21 +42,65 @@ class Venum(Module):
         targetIP = self.config.option('IP').value
         ipFile = self.config.option('IP_FILE').value
 
-        target = targetIP    
-        if targetIP == "xx.xx.xx.xx" or targetIP == "":
-            target = ipFile
-            if not os.path.exists(ipFile):
-                print("Missing input")
-                return 
+        if targetIP == "xx.xx.xx.xx" or targetIP is None:
+            targetIP = ipFile
+            if targetIP[:7] != "file://":
+                print("File input must have file:// in the beginning")
+                return None
 
-        if targetIP[:7] == "file://":
-            pass
+            ipAddresses = []
+            for ip in open(targetIP[7:]):
+                ipAddr = ip.strip()
+                if "#" in ipAddr:
+                    continue
 
-    def _venum(self,d,ip,ua):
+                if ipAddr == "":
+                    continue
+
+                ipAddresses.append(ipAddr)
+
+            ip_cnt = len(ipAddresses)
+            plural_msg = "addresses" if ip_cnt > 1 else "address"
+            print(f"Exploring on {ip_cnt} IP {plural_msg}")
+            for ip in ipAddresses:
+                title = self._venum(VHOST,ip,UA)
+                if title is None:
+                    print(f"Failed on {ip}")
+                    continue
+                print(f"{ip} | {title}")
+
+            return
+
+        title = self._venum(VHOST,targetIP,UA)
+        if title is None:
+            print(f"Failed on {targetIP}")
+            return
+
+        print(f"{targetIP} | {title}")
+
+    def _venum(self,d,ip,ua,https=False):
         ch = {
             "User-Agent": ua,
             "Host": d
         }
 
-        
-        pass
+        pattern = re.compile("<title.*?>(.+?)</title>")
+        scheme = "http://"
+
+        try:
+            if https:
+                scheme = "https://"
+            
+            # deepcode ignore SSLVerificationBypass: I know that
+            r = requests.get(f"{scheme}{ip}", headers=ch, verify=False)
+            title = re.findall(pattern, r.text)
+        except Exception as e:
+            if not https:
+                if DEBUG:
+                    print("Recursion!!!")
+                self._venum(d,ip,ua,https=True)
+
+            title = None
+            logging.error("[venum] {}".format(e))
+            
+        return title
